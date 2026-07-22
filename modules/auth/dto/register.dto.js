1837933bb712ef2e6f9a0fdd2d248526d5a4ca01
Your Full Name <hspatil000@gmail.com>
2026-07-23 01:54:57 +0530

    DTO registered

---
+++ modules/auth/dto/register.dto.js
@@ -0,0 +1,15 @@
+import Joi from "joi";
+import BaseDto from "../../../common/dto/base.dto";
+
+class RegisterDto extends BaseDto{
+    static schema = Joi.object({
+        name: Joi.string().trim().min(2).max(50).required(),
+        email:Joi.string().email().lowercase().required(),
+        password: Joi.string().min(8).required().message("Password must contain 8 chars"),
+        role: Joi.string().valid("customer","seller").default("customer")
+
+
+    })
+}
+
+export default RegisterDto
