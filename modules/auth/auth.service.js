90f6c52a5ae0ef80c50c58bbdfd18acd91211ab2
Your Full Name <hspatil000@gmail.com>
2026-08-03 01:57:24 +0530

    JWT added , Logout and forgotPassword Added

--- modules/auth/auth.service.js
+++ modules/auth/auth.service.js
@@ -1,71 +1,64 @@
-import ApiError from "../../common/utils/api-error.js"
-import { generateAccessToken, generateRefreshToken, generatteResetToken } from "../../common/utils/jwt-utils.js";
-import User from "./auth.models.js" //we do not call user scehma we call User always?
+import ApiError from "../../common/utils/api-error.js";
+import {
+  generateAccessToken,
+  generateRefreshToken,
+  generateResetToken,
+  verifyRefreshToken,
+} from "../../common/utils/jwt.utils.js";
+import User from "./auth.model.js";
 
+const hashToken = (token) =>
+  crypto.createHash("sha256").update(token).digest("hex");
 
-const hasheToken = (token) => crypto
-    .createHash("sha256")
-    .update(rawToken)
-    .digest("hex")
+const register = async ({ name, email, password, role }) => {
+  const existing = await User.findOne({ email });
+  if (existing) throw ApiError.conflict("Email already exisits");
 
-const register = async ({name, email, password, role})=>{
-// do user registeration 
-    const existing = await User.findOne({
-        email
-    })
-    if(existing){
-        throw ApiError.conflict("Email already Exists");
-    }
+  const { rawToken, hashedToken } = generateResetToken();
 
-    const {rawToken, hasheToken} = generateResetToken()
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+    verificationToken: hashedToken,
+  });
 
-    const user = await User.create({
-        name,
-        email,
-        password,
-        role,
-        verificationToken:hashedToken
-    })
+  // TODO: send an email to user with token: rawToken
 
-    //send an email to user with token : rawToken
+  const userObj = user.toObject();
+  delete userObj.password;
+  delete userObj.verificationToken;
 
-    const userObj = user.toObject()
-    delete userObj.password
-    delete userObj.verificationToken
-    return userObj
-}
+  return userObj;
+};
 
-const login = async ({email, password}) =>{
-    //take email and final user in DB
-    //then check if password is correct
-    //check if verified or not
+const login = async ({ email, password }) => {
+  //take email and find user in DB
+  // then check if password is correct
+  // check if verified or not
 
-    const user = await User.findOne({email}).select("+password")
-    if(!user) throw ApiError.unauthorized("Invalid email or password")
-    
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) throw ApiError.unauthorized("Invalid Email or password");
 
-    //somehow  i will check password
+  // somehow I will check password
 
+  if (!user.isVerified) {
+    throw ApiError.forbidden("Please verify your email before loggin");
+  }
 
-    if(!user.isVerified){
-        throw ApiError.forbidden("PLease verify you email before login")
-    }
+  const accessToken = generateAccessToken({ id: user._id, role: user.role });
+  const refreshToken = generateRefreshToken({ id: user._id });
 
-    const accessToken = generateAccessToken({id:user._id, role: user.role});
-    const refreshToken = generateRefreshToken({id: user._id})
+  user.refreshToken = hashToken(refreshToken);
+  await user.save({ validateBeforeSave: false });
 
-    user.refreshtoken = hasheToken(refreshToken)
+  const userObj = user.toObject();
+  delete userObj.password;
+  delete userObj.refreshToken;
 
-    await user.save({validateBeforeSave: false})
-    const userObj = user.toObject()
-    delete userObj.password
-    delete userObj.refreshToken
-    
-
-
-    return {user:userObj, accessToken, refreshToken}
-
-}
+  return { user: userObj, accessToken, refreshToken };
+};
 
 const refresh = async (token) => {
   if (!token) throw ApiError.unauthorized("Refresh token missing");
@@ -83,6 +76,27 @@ const refresh = async (token) => {
   return { accessToken };
 };
 
-export {register}
+const logout = async (userId) => {
+  //   const user = await User.findById(userId);
+  //   if (!user) throw ApiError.unauthorized("User not found");
 
-//so all logic is in service like controler passes request so services has logics like user is already present in db or not if not then create new if yes then login like this give 201 response like this
+  //   user.refreshToken = undefined;
+  //   await user.save({ validateBeforeSave: false });
+
+  await User.findByIdAndUpdate(userId, { refreshToken: null });
+};
+
+const forgotPassword = async (email) => {
+  const user = await User.findOne({ email });
+  if (!user) throw ApiError.notfound("No acccount with that email");
+
+  const { rawToken, hashedToken } = generateResetToken();
+  user.resetPasswordtoken = hashedToken;
+  user.resetpasswordExpires = Date.now() + 15 * 60 * 1000;
+
+  await user.save();
+
+  //TODO: mail bhejna nhi aata
+};
+
+export { register };
